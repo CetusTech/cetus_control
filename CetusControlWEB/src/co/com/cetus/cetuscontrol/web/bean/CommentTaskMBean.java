149eb1553954549be604c513cb64ca9849d2210f
jdavids87 <jose.salcedo@cetustechnology.co>
2016-07-22 21:29:34 -0500

    Desarrollo de la tarea de comentarios

--- CetusControlWEB/src/co/com/cetus/cetuscontrol/web/bean/CommentTaskMBean.java
+++ CetusControlWEB/src/co/com/cetus/cetuscontrol/web/bean/CommentTaskMBean.java
@@ -13,6 +13,8 @@ import co.com.cetus.cetuscontrol.dto.TaskDTO;
 import co.com.cetus.cetuscontrol.web.util.ConstantWEB;
 import co.com.cetus.common.dto.ResponseDTO;
 import co.com.cetus.common.util.UtilCommon;
+import co.com.cetus.common.validation.EmailValidator;
+import co.com.cetus.messageservice.ejb.service.SendMailRequestDTO;
 
 /**
  * The Class CommentTaskMBean.
@@ -24,12 +26,13 @@ import co.com.cetus.common.util.UtilCommon;
 @RequestScoped
 public class CommentTaskMBean extends GeneralManagedBean implements Serializable {
   
-  private static final long      serialVersionUID = 1L;
-                                                  
-  private List< CommentTaskDTO > listComment      = null;
-  
-  private CommentTaskDTO commentTaskDTO = null;
-                                                  
+  private static final long      serialVersionUID      = 1L;
+                                                       
+  private List< CommentTaskDTO > listComment           = null;
+  private CommentTaskDTO         commentTaskDTO        = null;
+  private boolean                showAddComment        = false;
+  private boolean                disabledBtnAddComment = false;
+                                                       
   public CommentTaskMBean () {
     commentTaskDTO = new CommentTaskDTO();
   }
@@ -44,16 +47,16 @@ public class CommentTaskMBean extends GeneralManagedBean implements Serializable
   @PostConstruct
   @Override
   public void initElement () {
-    ResponseDTO responseDTO = null; 
+    ResponseDTO responseDTO = null;
     try {
-      if( getUserDTO() != null ){
+      if ( getUserDTO() != null ) {
         TaskDTO task = ( TaskDTO ) getObjectSession( "selectedObject" );
         if ( task != null ) {
           responseDTO = generalDelegate.findCommentByIdTask( task.getId() );
-          if( UtilCommon.validateResponseSuccess( responseDTO ) ){
+          if ( UtilCommon.validateResponseSuccess( responseDTO ) ) {
             listComment = ( List< CommentTaskDTO > ) responseDTO.getObjectResponse();
-          }else{
-            listComment = new ArrayList<>();
+          } else {
+            listComment = new ArrayList< >();
           }
           
           addObjectSession( listComment, "listComment" );
@@ -67,13 +70,72 @@ public class CommentTaskMBean extends GeneralManagedBean implements Serializable
     
   }
   
-  public void validateAdd(){
+  /**
+   * </p> Validate add. </p>
+   *
+   * @author Jose David Salcedo M. - Cetus Technology
+   * @since CetusControlWEB (12/06/2016)
+   */
+  public void validateAdd () {
+    String[] vecEmail = null;
+    String emailError = null;
+    ResponseDTO responseDTO = null;
     try {
-      
-    } catch ( Exception e ) {
+      if ( commentTaskDTO != null ) {
+        vecEmail = commentTaskDTO.getReceiver().split( ";" );
+        if ( vecEmail != null && vecEmail.length > 0 ) {
+          for ( String email: vecEmail ) {
+            if ( !EmailValidator.validate( email ) ) {
+              emailError += email;
+            }
+          }
+          if ( emailError != null ) {
+            addMessageError( "idTo", ConstantWEB.MESSAGE_ERROR, emailError );
+          } else {
+            ConstantWEB.WEB_LOG.info( "Se procede a enviar el comentario..." );
+            
+            commentTaskDTO.setCreationDate( getCurrentDateTime() );
+            TaskDTO task = ( TaskDTO ) getObjectSession( "selectedObject" );
+            commentTaskDTO.setTask( task );
+            commentTaskDTO.setEmitter( task.getPersonGroup().getPerson().getLastNames() + " " + task.getPersonGroup().getPerson().getNames() + " ("
+                                       + task.getPersonGroup().getPerson().getEmail() + ")" );
+                                       
+            ConstantWEB.WEB_LOG.info( commentTaskDTO.toString() );
+            
+            SendMailRequestDTO sendMailRequestDTO = new SendMailRequestDTO();
+            sendMailRequestDTO.setUser( generalDelegate.getValueParameter( ConstantWEB.USER_WS_MESSAGE_SERVICE ) );
+            sendMailRequestDTO.setPassword( generalDelegate.getValueParameter( ConstantWEB.PASSWORD_WS_MESSAGE_SERVICE ) );
+            sendMailRequestDTO.setMessage( commentTaskDTO.getCommentT() );
+            sendMailRequestDTO.setSubject( commentTaskDTO.getSubject() );
+            sendMailRequestDTO.setRecipients( vecEmail );
+            
+            responseDTO = generalDelegate.sendEmail( sendMailRequestDTO );
+            
+            if ( UtilCommon.validateResponseSuccess( responseDTO ) ) {
+              generalDelegate.create( commentTaskDTO );
+              addMessageInfo( null, ConstantWEB.MESSAGE_SUCCES, ConstantWEB.MSG_DETAIL_SUCCESS );
+            } else {
+              addMessageError( null, ConstantWEB.MESSAGE_ERROR_CREATE, ConstantWEB.MSG_DETAIL_ERROR );
+            }
+          }
+        }
+      }
+    } catch (
+    
+    Exception e )
+    
+    {
       ConstantWEB.WEB_LOG.error( e.getMessage(), e );
     }
+    
   }
+  
+  public void openComment () {
+    commentTaskDTO = new CommentTaskDTO();
+    showAddComment = true;
+    disabledBtnAddComment = true;
+  }
+
   
   public List< CommentTaskDTO > getListComment () {
     return listComment;
@@ -100,15 +162,29 @@ public class CommentTaskMBean extends GeneralManagedBean implements Serializable
     // TODO Auto-generated method stub
     return null;
   }
-
+  
   public CommentTaskDTO getCommentTaskDTO () {
     return commentTaskDTO;
   }
-
+  
   public void setCommentTaskDTO ( CommentTaskDTO commentTaskDTO ) {
     this.commentTaskDTO = commentTaskDTO;
   }
-
   
+  public boolean isShowAddComment () {
+    return showAddComment;
+  }
+  
+  public void setShowAddComment ( boolean showAddComment ) {
+    this.showAddComment = showAddComment;
+  }
+  
+  public boolean isDisabledBtnAddComment () {
+    return disabledBtnAddComment;
+  }
+  
+  public void setDisabledBtnAddComment ( boolean disabledBtnAddComment ) {
+    this.disabledBtnAddComment = disabledBtnAddComment;
+  }
   
 }
